cf986be8ebecdb692ae4f6080dbb54c5722f4d0b
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2023-12-09 21:38:47 +0100

    Change kind icons

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -15,5 +15,39 @@ return {
             opts.sections.lualine_z = { 'location' }
         end
     },
+    {
+        'LazyVim/LazyVim',
+        opts = {
+            icons = {
+                kinds = {
+                    Text = "󰉿 ",
+                    Method = "󰆧 ",
+                    Function = "󰊕 ",
+                    Constructor = " ",
+                    Field = "󰜢 ",
+                    Variable = "󰀫 ",
+                    Class = "󰠱 ",
+                    Interface = " ",
+                    Module = " ",
+                    Property = "󰜢 ",
+                    Unit = "󰑭 ",
+                    Value = "󰎠 ",
+                    Enum = " ",
+                    Keyword = "󰌋 ",
+                    Snippet = " ",
+                    Color = "󰏘 ",
+                    File = "󰈙 ",
+                    Reference = "󰈇 ",
+                    Folder = "󰉋 ",
+                    EnumMember = " ",
+                    Constant = "󰏿 ",
+                    Struct = "󰙅 ",
+                    Event = " ",
+                    Operator = "󰆕 ",
+                    TypeParameter = "",
+                }
+            },
+        }
+    },
 }
 
